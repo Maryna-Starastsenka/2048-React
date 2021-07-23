dcc1a2afd55d72aeb7c8f8d09906e2395948d631
VladStarostenko <vlad.starostenko.0501@gmail.com>
2021-07-23 16:41:24 +0200

    Add login form

--- 2048/src/components/Login/Login.js
+++ 2048/src/components/Login/Login.js
@@ -1,14 +1,114 @@
 class Login extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            username: "",
+            password: "",
+            formErrors: [],
+        };
+    }
+
+    handleChange = (e) => {
+        const { id, value } = e.target;
+        this.setState((prevState) => ({
+            ...prevState,
+            [id]: value,
+            formErrors: prevState.formErrors.filter((error) => error !== id),
+        }));
+    };
+
+    hasError(key) {
+        return this.state.formErrors.indexOf(key) !== -1;
+    }
+
+    login = () => {
+        const formErrors = [];
+        if (this.state.username === "") {
+            formErrors.push("username");
+        }
+
+        if (this.state.password === "") {
+            formErrors.push("password");
+        }
+
+        this.setState({
+            formErrors,
+        });
+
+        if (formErrors.length > 0) {
+            return false;
+        } else {
+            console.log(this.state);
+            this.state.username = "";
+            this.state.password = "";
+        }
+    };
+
     render() {
         return (
             <div>
-                <Navigation/>
-                <div className='login_container'>
-                    Here will be login page :)
+                <Navigation />
+                <div className="container">
+                    <form className="login_form">
+                        <h3>Log in</h3>
+
+                        <div className="form-group">
+                            <label>Username</label>
+                            <input
+                                id="username"
+                                type="username"
+                                className={`form-control ${
+                                    this.hasError("username") ? "form-control--error" : ""
+                                }`}
+                                placeholder="Enter email"
+                                value={this.state.username}
+                                onChange={() => this.handleChange(event)}
+                            />
+                            <div
+                                className="form_error-message"
+                                style={{
+                                    visibility: this.hasError("username") ? "inherit" : "hidden",
+                                }}
+                            >
+                                Username is required!
+                            </div>
+                        </div>
+
+                        <div className="form-group">
+                            <label>Password</label>
+                            <input
+                                id="password"
+                                type="password"
+                                className={`form-control ${
+                                    this.hasError("password") ? "form-control--error" : ""
+                                }`}
+                                placeholder="Enter password"
+                                value={this.state.password}
+                                onChange={() => this.handleChange(event)}
+                            />
+                            <div
+                                className="form_error-message"
+                                style={{
+                                    visibility: this.hasError("password") ? "inherit" : "hidden",
+                                }}
+                            >
+                                Password is required!
+                            </div>
+                        </div>
+
+                        <button
+                            type="button"
+                            className="btn btn-dark btn-lg btn-block"
+                            onClick={() => this.login()}
+                        >
+                            Sign in
+                        </button>
+                    </form>
                 </div>
             </div>
         );
     }
 }
 
-ReactDOM.render(<Login/>, document.getElementById("root"));
+ReactDOM.render(<Login />, document.getElementById("root"));
