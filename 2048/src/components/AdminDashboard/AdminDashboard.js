e2ae7a48c6663f4b664794f8d6f9b20194c6be25
Maryna Sirotsina <maryna.sirotsina@gmail.com>
2021-07-24 00:41:34 -0400

    Dashboard : table template

--- 2048/src/components/AdminDashboard/AdminDashboard.js
+++ 2048/src/components/AdminDashboard/AdminDashboard.js
@@ -4,7 +4,36 @@ class AdminDashboard extends React.Component {
             <div>
                 <Navigation/>
                 <div className='admin-dashboard_container'>
-                    Here will be Admin Dashboard page :)
+                    <h3>Administrator Dashboard</h3>
+
+                    <h4>Players</h4>
+                    <table className="table table-success table-striped table-hover">
+                        <thead>
+                        <tr>
+                            <th>User ID</th>
+                            <th>Username</th>
+                            <th>Admin rights</th>
+                            <th>Best score</th>
+                            <th>Online</th>
+                            <th>Signup date</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        <tr>
+                            <td>1</td>
+                            <td>1</td>
+                            <td>1</td>
+                            <td>1</td>
+                            <td>1</td>
+                            <td>1</td>
+                        </tr>
+                        </tbody>
+                    </table>
+
+                    <h4>Player count</h4>
+
+                    <h4>Online players</h4>
                 </div>
             </div>
         );
